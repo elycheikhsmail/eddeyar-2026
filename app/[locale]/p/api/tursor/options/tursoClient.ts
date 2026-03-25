cdd270601f6a74ebae4301a49229f92b1d1d2cc5
ely cheikh <eouldsmail@gmail.com>
2026-03-25 13:02:28 +0000

    remove sqlite and turso code

--- app/[locale]/p/api/tursor/options/tursoClient.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "@libsql/client";
-export const turso = createClient({
-  url: process.env.TURSO_DATABASE_URL_OPTIONS!,
-  authToken: process.env.TURSO_AUTH_TOKEN_OPTIONS!,
-});
